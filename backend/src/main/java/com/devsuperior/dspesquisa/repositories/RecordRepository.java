f9feb2b71869a93e365e39985425e64042d3ce86
Juliano Hausen Oliveira da Costa <juliano.h.costa@rfb.gov.br>
2020-09-15 16:04:37 -0400

    ready for production

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -15,7 +15,7 @@ import com.devsuperior.dspesquisa.entities.Record;
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
 	@Query("SELECT obj FROM Record obj WHERE "
-			+"(coalesce(:min IS NULL) OR obj.moment >= :min) AND "
-			+"(coalesce(:max IS NULL) OR obj.moment <= :max)")
+			+"(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+"(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
 	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 }
